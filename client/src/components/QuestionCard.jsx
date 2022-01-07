daa73f9fec70dd8971a3ccbba48e8e326dc1cae2
valentinflegon <valfleg@gmail.com>
2022-01-08 00:51:45 +0100

    Front | Quizz DistCities avancement

--- client/src/components/QuestionCard.jsx
+++ client/src/components/QuestionCard.jsx
@@ -6,36 +6,27 @@ import TextField from '@mui/material/TextField';
 
 const NUMBERQUESTION = 10;
 
-QuestionCard.protoTypes = {
-  isStarted: PropTypes.bool
-}
 
-function QuestionCard({currentQuestion, question, inputText}){
-  let isStarted;
-  if (currentQuestion > 0) isStarted = true;
-  return isStarted ?(
-      <Card className='questionCard'>
-        <h2>Question 1/10: <br /> <span>{question}</span></h2>
 
-        <TextField
-          className='input'
-          id="outlined-number"
-          label="Réponse"
-          type="number"
-          placeholder= {inputText}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        /> 
-        <div className='button'>
-        <Button variant="contained">Next</Button>
-        </div>
-      </Card>   
-  ) : (
-    <Card className='questionCard'>
-<Button variant="contained">Start</Button>      
-    </Card>   
-  );
+function QuestionCard({currentQuestion, question, inputText, citie1, citie2}){
+  
+    return (
+        <Card className='questionCard'>
+          <h2>Question {currentQuestion}/10: <br /> <span>{question} {citie1} et {citie2} ?</span></h2>
+
+          <TextField
+            className='inputField'
+            id="outlined-number"
+            label="Réponse"
+            type="number"
+            placeholder= {inputText}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          /> 
+        
+        </Card>);
+
 }
 
 
--- client/src/pages/Game/DistCities.jsx
+++ client/src/pages/Game/DistCities.jsx
@@ -1,19 +1,76 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import QuestionCard from '../../components/QuestionCard';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import QuestionCard from "../../components/QuestionCard";
+import { Card, Button } from "@mui/material";
 
 const NUMBERQUESTION = 10;
 
 const DistCities = () => {
-    const currentQuestion = 1;
-  return (
-    <>
-    <h1>Quizz Distance Villes</h1>
-     
-     <QuestionCard currentQuestion={currentQuestion} question={'Quelle est la distance entre Marseille et Bordeaux ?'} inputText={'Distance en KM'} /> 
+  let { state } = useLocation();
+  console.log(state, "from game mod choice");
 
-    </>
-  );
+  const data = require("../../components/cities.json");
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const [cities, setCities] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  function loadCities() {
+    const nb = getRandomInt(data.length);
+    const citie1 = data[nb];
+    data.splice(nb, 1);
+    const nb2 = getRandomInt(data.length);
+    const citie2 = data[nb2];
+    data.splice(nb2, 1);
+    setCities([citie1, citie2]);
+    setCurrentQuestion([parseInt(currentQuestion) + 1]);
+  }
+
+  if (currentQuestion == 0) { //Pas encore commencé
+    return (
+      <>
+        <h1>Quizz Distance Villes</h1>
+
+        <div className="button">
+          <Button onClick={loadCities} variant="contained">
+            Commencer
+          </Button>
+        </div>
+      </>
+    );
+  }
+  if (currentQuestion == 11) { // Quizz fini
+    return (
+      <>
+        <h1>Quizz Distance Villes</h1>
+
+        <div className="button">Finito </div>
+      </>
+    );
+  } else
+    return (
+      <>
+        <h1>Quizz Distance Villes</h1>
+
+        {cities && currentQuestion && (
+          <QuestionCard
+            currentQuestion={currentQuestion}
+            question={"Quelle est la distance entre "}
+            citie1={cities[0]}
+            citie2={cities[1]}
+            inputText={"Distance en KM"}
+          />
+        )}
+        <div className="button">
+          <Button onClick={loadCities} variant="contained">
+            Next
+          </Button>
+        </div>
+      </>
+    );
 };
 
 export default DistCities;
