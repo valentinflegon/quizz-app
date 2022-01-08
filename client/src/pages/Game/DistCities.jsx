2dbc3dabab7e94968bfa1f768a7ddb18502e964c
valentinflegon <valfleg@gmail.com>
2022-01-08 12:29:28 +0100

    FRONT | Tab fin de game

--- client/src/pages/Game/DistCities.jsx
+++ client/src/pages/Game/DistCities.jsx
@@ -3,14 +3,37 @@ import { useParams } from "react-router-dom";
 import { NavLink, useLocation } from "react-router-dom";
 import QuestionCard from "../../components/QuestionCard";
 import { Card, Button } from "@mui/material";
-import '../../styles/components/_DistCities.scss'
-import TextField from '@mui/material/TextField';
-
+import "../../styles/components/_DistCities.scss";
+import TextField from "@mui/material/TextField";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const NUMBERQUESTION = 10;
 
 const DistCities = () => {
   let { state } = useLocation();
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
 
   const data = require("../../components/cities.json");
   function getRandomInt(max) {
@@ -30,9 +53,26 @@ const DistCities = () => {
     setCities([citie1, citie2]);
 
     setCurrentQuestion([parseInt(currentQuestion) + 1]);
+    // fetch('https://fr.distance24.org/route.json?stops=Hamburg|Berlin',{
+    //     method:'GET',
+    //     headers:{
+    //         "Accept": "application/json",
+    //         "Content-Type": 'application/json',
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Headers': 'Content-Type',
+    //   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+    //   'Authorization': 'Bearer key'
+    //     }
+    // })
+    // .the(resp => { return resp.json();})
+    // .then(responseData => {console.log(responseData); return responseData;})
+    // .then(data => {this.setState({"questions" : data});})
+    // .catch(err => {
+    //     console.log("fetch error" + err);
+    // });
   }
 
-  function newGame(){
+  function newGame() {
     setCurrentQuestion([parseInt(0) + 1]);
   }
 
@@ -40,8 +80,8 @@ const DistCities = () => {
     return (
       <>
         <h1>Quizz Distance Villes</h1>
-        <Card className='startCard' >
-            <div className="button">
+        <Card className="startCard">
+          <div className="button">
             <Button onClick={loadCities} variant="contained">
               Commencer
             </Button>
@@ -54,41 +94,78 @@ const DistCities = () => {
     return (
       <>
         <h1>Quizz Distance Villes</h1>
-        <Card className='startCard' >
-            
-            <div>Tab score </div>
-            <Button onClick={loadCities,newGame} variant="contained">
-            Next
+          <TableContainer className='tab'component={Paper}>
+            <Table
+              sx={{ minWidth: 650 }}
+              size="small"
+              aria-label="a dense table"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell>Question</TableCell>
+                  <TableCell align="right">Précision</TableCell>
+                  <TableCell align="right">Votre réponse</TableCell>
+                  <TableCell align="right">Correction</TableCell>
+                  <TableCell align="right">Score</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        <div className="buttonEnd"> 
+            <Button onClick={(loadCities, newGame)}  variant="contained">
+            Recommencer
           </Button>
-        </Card>
+        </div>
+         
       </>
     );
-  } 
-  else
+  } else
     return (
       <>
         <h1>Quizz Distance Villes</h1>
 
-        <Card className='questionCard'>
-          <h2>Question {currentQuestion}/10: <br /> <span>Quelle est la distance entre  {cities[0]} et {cities[1]} ?</span></h2>
+        <Card className="questionCard">
+          <h2>
+            Question {currentQuestion}/10: <br />{" "}
+            <span>
+              Quelle est la distance entre {cities[0]} et {cities[1]} ?
+            </span>
+          </h2>
 
           <TextField
-            className='inputField'
+            className="inputField"
             id="outlined-number"
             label="Réponse"
             type="number"
-            placeholder= "Distance en KM"
+            placeholder="Distance en KM"
             InputLabelProps={{
               shrink: true,
             }}
-          /> 
-        
-        <div className="button">
-          <Button onClick={loadCities} variant="contained">
-            Next
-          </Button>
-        </div>
-        </Card></>
+          />
+
+          <div className="button">
+            <Button onClick={loadCities} variant="contained">
+              Next
+            </Button>
+          </div>
+        </Card>
+      </>
     );
 };
 
