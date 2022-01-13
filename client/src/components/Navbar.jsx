0817338ee1a1aa9a87c95ed102ba4e95e5bb1ce0
kgeoffroy <kirsan.geoffroy@etu.u-bordeaux.fr>
2022-01-13 22:40:32 +0100

    ajout d'un avatar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../assets/logo.svg";
+import avatar from "../assets/avatar.png";
 import { NavLink } from "react-router-dom";
 import "../styles/components/_navbar.scss";
 import { useAppContext } from "../lib/contextLib";
@@ -126,7 +127,7 @@ const NavBar = () => {
             <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  <Avatar alt="avatar" src={avatar} />
                 </IconButton>
               </Tooltip>
               <Menu
