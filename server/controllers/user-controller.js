827c6688f01924eae1175fbb82a0f4f5867cc632
kgeoffroy <kirsan.geoffroy@etu.u-bordeaux.fr>
2022-01-14 11:54:37 +0100

    Documentation swagger terminé

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,54 +3,52 @@ const bcrypt = require('bcryptjs');
 
 createUser = async (req, res) => {
   const { body } = req;
+  if (!body) {
+    return res.status(404).json({
+      success: false,
+      error: 'You must provide a valid body'
+    })
+  }
   const { username, email } = body;
   try {
     const usernameExist = await User.findOne({ username: username });
-    const emailExist = await User.findOne({ email: email });
     if (usernameExist) {
-      return res.status(200).json({
+      return res.status(404).json({
         success: false,
         message: { username: "Username already exist" }
       });
     }
+    const emailExist = await User.findOne({ email: email });
     if (emailExist) {
-      return res.status(200).json({
+      return res.status(404).json({
         success: false,
         message: { email: "Email already exist" }
       });
     }
   } catch (error) {
-    return res.json({
+    return res.status(404).json({
       success: false,
       message: "failed to load data to compare with"
     });
   }
 
-  if (!body) {
-    return res.status(400).json({
-      success: false,
-      error: 'You must provide a user',
-    })
-  }
-
   const user = new User(body);
-
   if (!user) {
-    return res.status(400).json({ success: false, error: err });
+    return res.status(404).json({ success: false, error: err });
   }
 
   user.save()
     .then(() => {
-      return res.status(201).json({
+      return res.status(200).json({
         success: true,
         data: user,
-        message: 'User created!',
+        message: 'User created!'
       });
     })
     .catch(error => {
-      return res.status(400).json({
-        error,
-        message: 'User not created!',
+      return res.status(404).json({
+        success: false,
+        message: 'User not created, make sure to check all required field!'
       });
     });
 }
@@ -133,7 +131,7 @@ getUserById = async (req, res) => {
     });
   } catch (err) {
     return res.status(404).json({
-      success:false,
+      success: false,
       data: "User by id not found"
     });
   }
@@ -143,7 +141,7 @@ getUserById = async (req, res) => {
 getUserByUsername = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username });
-    if(!user) return res.sendStatus(404).json({
+    if (!user) return res.status(404).json({
       success: false,
       data: "User by username not found"
     });
@@ -152,7 +150,10 @@ getUserByUsername = async (req, res) => {
       data: user
     });
   } catch (err) {
-    return res.sendStatus(404);
+    return res.status(404).json({
+      success: false,
+      data: "User by username not found"
+    });
   }
 }
 
@@ -175,23 +176,24 @@ getUsers = async (req, res) => {
 //Log in
 logIn = async (req, res) => {
   const { username, password } = req.body;
-  // console.log(username, password);
   const user = await User.findOne({ username: username });
-  if (user == undefined)
-    return res.status(200).json({
-      success: false,
-      message: "username inconnu"
-    });
+  if (!user) return res.status(404).json({
+    success: false,
+    message: "User was not found"
+  });
   else {
     bcrypt.compare(password, user.password).then(
       (response) => {
-        if (!response)
-          return res.status(200).json({
-            success: response,
-            message: "Username and password does not match !"
-          });
+        if (!response) return res.status(404).json({
+          success: false,
+          message: "response from bcrypt compare failed"
+        });
         else {
-          return res.status(200).json({success: true});
+          return res.status(200).json({
+            success: true,
+            data: user,
+            message: "User logged"
+          });
         }
       }
     );
--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -10,7 +10,6 @@ const userCtrl = require('../controllers/user-controller');
  *          required: 
  *              - username
  *              - password
- *              - email
  *          properties:
  *              id:
  *                  type: string
@@ -24,17 +23,200 @@ const userCtrl = require('../controllers/user-controller');
  *              email:
  *                  type: email
  *                  description: The user email
+ *              scores: 
+ *                  type: object
+ *                  properties:
+ *                      distancePays:
+ *                          type: array
+ *                          items: 
+ *                              type: integer
+ *                              description: The score for the quizz of distance between countries
+ *                      distanceVilles:
+ *                          type: array 
+ *                          items: 
+ *                              type: integer
+ *                              description: The score for the quizz of distance between cities
+ *                      populationPays: 
+ *                          type: array
+ *                          items: 
+ *                              type: integer
+ *                              description: The score for the quizz of population between countries
+ *                      populationVilles:
+ *                          type: array
+ *                          items: 
+ *                              type: integer
+ *                              description: The score for the quizz of population between cities
  *          example: 
  *              id: d5fE_asz
  *              username: Toto
  *              password: toto123
  *              email: toto@toto.fr
+ *              scores:
+ *                  distancePays: [100, 200, 963]
+ *                  distanceVilles: [0]
+ *                  populationPays: []
+ *                  populationVilles: [1000,120,165]
  */
 
+/**
+ * @openapi
+ * /api/user:
+ *  post:
+ *      summary: Create a new user 
+ *      tags: [Users]
+ *      requestBody:
+ *          required: true
+ *          email: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses: 
+ *          200:
+ *              description: User created successfully
+ *          404:
+ *              description: The user was not found
+ *          500:
+ *              description: Some error server
+ */
 router.post('/user', userCtrl.createUser);
+
+/**
+ * @openapi
+ * /api/login:
+ *  post:
+ *      summary: Log a user 
+ *      tags: [Users]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses: 
+ *          200:
+ *              description: Logged successfully
+ *          404:
+ *              description: The user was not found
+ *          500:
+ *              description: Some error server
+ */
+router.post('/login', userCtrl.logIn);
+
+/**
+ * @openapi
+ * /api/user/{id}:
+ *  put:
+ *      summary: Update the user by the id 
+ *      tags: [Users]
+ *      parameters: 
+ *        - in: path
+ *          name: id
+ *          schema: 
+ *              type: string
+ *          required: true
+ *          description: The user id
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: The user was updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          404:
+ *              description: The user was not found
+ *          500:
+ *              description: Some error server
+ */
 router.put('/user/:id', userCtrl.updateUser);
+
+/**
+ * @openapi
+ * /api/add-score/{id}:
+ *  put:
+ *      summary: Update the user score by the id 
+ *      tags: [Users]
+ *      parameters: 
+ *        - in: path
+ *          name: id
+ *          schema: 
+ *              type: string
+ *          required: true
+ *          description: The user id
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: The user score was updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          404:
+ *              description: The user was not found
+ *          500:
+ *              description: Some error server
+ */
 router.put('/add-score/:id', userCtrl.addScore);
+
+/**
+ * @openapi
+ * /api/user/{id}:
+ *  delete:
+ *      summary: Delete the user by id
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema: 
+ *              type: string
+ *            required: true
+ *            description: The user id
+ *      responses:
+ *          200:
+ *              description: The delete user by id 
+ *          contents:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *          400:
+ *              description: The user was not delete by id
+ */
 router.delete('/user/:id', userCtrl.deleteUser);
+
+/**
+ * @openapi
+ * /api/user-username/{username}:
+ *  delete:
+ *      summary: Delete the user by username
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: username
+ *            schema: 
+ *              type: string
+ *            required: true
+ *            description: The user username
+ *      responses:
+ *          200:
+ *              description: Delete the user by username
+ *          contents:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *          400:
+ *              description: The delete user by username failed
+ */
 router.delete('/user-username/:username', userCtrl.deleteUserByUsername);
 
 /**
@@ -61,6 +243,29 @@ router.delete('/user-username/:username', userCtrl.deleteUserByUsername);
  *              description: The user was not found
  */
 router.get('/user/:id', userCtrl.getUserById);
+/**
+ * @openapi
+ * /api/user-username/{username}:
+ *  get:
+ *      summary: Get the user by username
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: username
+ *            schema: 
+ *              type: string
+ *            required: true
+ *            description: The user username
+ *      responses:
+ *          200:
+ *              description: The user description by username 
+ *          contents:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *          400:
+ *              description: The user was not found by username
+ */
 router.get('/user-username/:username', userCtrl.getUserByUsername);
 
 /**
@@ -87,6 +292,5 @@ router.get('/user-username/:username', userCtrl.getUserByUsername);
  *                              $ref: '#/components/schemas/User'
  */
 router.get('/users', userCtrl.getUsers);
-router.post('/login', userCtrl.logIn);
 
 module.exports = router;
