f586a03dbfe4033b738332c3a6f8db7bbeac887e
valentinflegon <valfleg@gmail.com>
2022-01-16 21:50:05 +0100

    profile name et avatar

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,9 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import "../styles/components/_profile.scss";
+import { IconButton, Avatar } from "@mui/material";
+import avatar from "../assets/avatar.png";
+
 
 const Profile = () => {
   const user = useUserContext();
@@ -19,18 +22,30 @@ const Profile = () => {
     user.user.scores.distancePays
   );
 
-  listDistCities.sort((a,b) => b - a);
-  listDistCountries.sort((a,b) => b - a);
-
-
+  listDistCities.sort((a, b) => b - a);
+  listDistCountries.sort((a, b) => b - a);
 
   return (
     <>
       {" "}
-      <div className="profile">
-        <div>
-          <p>Profile image : </p>
-          <p>Profile username : {user.user.username}</p>
+      <div>
+        <div className="Tab profile">
+          <TableContainer component={Paper}>
+            <Table size="small" aria-label="a dense table">
+              <TableBody>
+                <div>
+                <IconButton sx={{ p: 4 }}>
+                  <Avatar alt="avatar" src={avatar} />
+                </IconButton>
+                </div>
+                <div className="name">
+                <b>{user.user.username}</b>
+                </div>
+             
+               
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       </div>
       <div className="Tab">
@@ -44,7 +59,7 @@ const Profile = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {listDistCities.slice(0,5).map((row) => (
+              {listDistCities.slice(0, 5).map((row) => (
                 <TableRow
                   key={row.name}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -61,14 +76,16 @@ const Profile = () => {
       </div>
       <div className="Tab ">
         <TableContainer component={Paper}>
-          <Table sx={{ }} size="small" aria-label="a dense table">
+          <Table sx={{}} size="small" aria-label="a dense table">
             <TableHead>
               <TableRow>
-                <TableCell><b>Score distance Pays:</b></TableCell>
+                <TableCell>
+                  <b>Score distance Pays:</b>
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {listDistCountries.slice(0,5).map((row) => (
+              {listDistCountries.slice(0, 5).map((row) => (
                 <TableRow
                   key={row.name}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
