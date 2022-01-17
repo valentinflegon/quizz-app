73041a6bc32ee9eaddba021acb80335a014dbc28
valentinflegon <valfleg@gmail.com>
2022-01-17 15:36:30 +0100

    ajout leaderboard distVille et cities

--- client/src/pages/Leaderboard.jsx
+++ client/src/pages/Leaderboard.jsx
@@ -12,22 +12,38 @@ import { useUsersContext } from "../lib/contextLib";
 import "../styles/_leaderboard.scss"
 
 export default function BasicTable() {
-  const rows = [];
-  const [array, setArray] = useState([]);
-  const onChange = ({ target }) => setArray(target.array);
+  const rowsDistCities = [];
+  const [arrayDistCities, setArrayDistCities] = useState([]);
+
+  const rowsDistCountries = [];
+  const [arrayDistCountries, setArrayDistCountries] = useState([]);
+
+  const onChangeDistCities = ({ target }) => setArrayDistCities(target.arrayDistCities);
+  const onChangeDistCountries = ({ target }) => setArrayDistCountries(target.arrayDistCoutries);
+
   const { setUsers } = useUsersContext();
   try {
     axios.get("http://localhost:8080/api/users/").then((response) => {
       const { data } = response;
       if (data.success) {
         data.data.map((userObject) => {
-          let max = 0;
+          let maxDistCountries = 0;
+          let maxDistCities = 0
           for (let i = 0; i < userObject.scores.distancePays.length; i++) {
-            if (userObject.scores.distancePays[i] > max)
-              max = userObject.scores.distancePays[i];
+            if (userObject.scores.distancePays[i] > maxDistCountries)
+              maxDistCountries = userObject.scores.distancePays[i];
           }
-          rows.push({ username: userObject.username, score: max });
-          rows.sort((a, b) => b.score - a.score);
+          rowsDistCountries.push({ username: userObject.username, score: maxDistCountries });
+          rowsDistCountries.sort((a, b) => b.score - a.score);
+
+          for (let i = 0; i < userObject.scores.distanceVilles.length; i++) {
+            if (userObject.scores.distanceVilles[i] > maxDistCities)
+              maxDistCities = userObject.scores.distanceVilles[i];
+          }
+          rowsDistCities.push({ username: userObject.username, score: maxDistCities });
+          rowsDistCities.sort((a, b) => b.score - a.score);
+
+          
         });
         setUsers(data.data);
       } else {
@@ -37,37 +53,72 @@ export default function BasicTable() {
   } catch (e) {
     console.log(e);
   }
-  useEffect(() => setArray(rows), array);
+  useEffect(() => setArrayDistCountries(rowsDistCountries), arrayDistCountries);
+  useEffect(() => setArrayDistCities(rowsDistCities), arrayDistCities);
+
     return (
-    <div className="Tab">
-      <p><strong>LeaderBoard Distance Villes : </strong></p>
-      <TableContainer onChange={{onChange}} sx={{width: 400}}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-            <TableCell align="center"><b>Classement</b></TableCell>
-              <TableCell align="center"><b>Username</b></TableCell>
-              <TableCell align="center"><b>Score&nbsp;(pts)</b></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {array.slice(0,10).map((row,index) => (
-              <TableRow
-                key={row.usernamename}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {index+1}
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {row.username}
-                </TableCell>
-                <TableCell align="right">{row.score}</TableCell>
+    <><div className="Tab">
+        <p><strong>LeaderBoard Distance Pays: </strong></p>
+        <TableContainer onChange={{ onChangeDistCountries}} sx={{ width: 400 }}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center"><b>Classement</b></TableCell>
+                <TableCell align="center"><b>Username</b></TableCell>
+                <TableCell align="center"><b>Score&nbsp;(pts)</b></TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>);
+            </TableHead>
+            <TableBody>
+              {arrayDistCountries.slice(0, 10).map((rowsDistCountries, index) => (
+                <TableRow
+                  key={rowsDistCountries.usernamename}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {index + 1}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {rowsDistCountries.username}
+                  </TableCell>
+                  <TableCell align="right">{rowsDistCountries.score}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div className="Tab">
+          <p><strong>LeaderBoard Distance Villes: </strong></p>
+          <TableContainer onChange={{ onChangeDistCities }} sx={{ width: 400 }}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center"><b>Classement</b></TableCell>
+                  <TableCell align="center"><b>Username</b></TableCell>
+                  <TableCell align="center"><b>Score&nbsp;(pts)</b></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {arrayDistCities.slice(0, 10).map((rowsDistCities, index) => (
+                  <TableRow
+                    key={rowsDistCities.usernamename}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {index + 1}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {rowsDistCities.username}
+                    </TableCell>
+                    <TableCell align="right">{rowsDistCities.score}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        </>
+    
+    );
 
 }
